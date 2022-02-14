3d346df18dfaac41f337ba970823833e0a052fce
KasunHasanga <kasunhasanga96@gmail.com>
2022-02-14 22:00:24 +0530

    change theme: Darkmode and light mode

---
+++ lib/ui/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+const Color bluishClr = Color(0xFF4e5ae8);
+const Color yelloClr = Color(0xFFFFB746);
+const Color pinkClr = Color(0xFFff4667);
+const Color white = Colors.white;
+const primryClr = bluishClr;
+const Color darkGrayClr = Color(0xFF121212);
+const Color darkHeaderClr = Color(0xFF424242);
+
+class Themes {
+  static final light =
+      ThemeData(primaryColor: primryClr, brightness: Brightness.light);
+
+  static final dark =
+      ThemeData(primaryColor: darkGrayClr, brightness: Brightness.dark);
+}
